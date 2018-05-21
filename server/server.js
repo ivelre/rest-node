6f37669571d7c8c14a95337efb5cac2a2bf8e7a2
ivelre <ivelre@hotmail.com>
2018-05-21 15:02:59 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require('./config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.get('/usuario',function(req,res){
+	res.send('Hello word get')
+})
+app.post('/usuario',function(req,res){
+	let body = req.body
+	if(body.nombre === undefined)
+		res.status(400).json({
+			status:false,
+			message:'not found name.'
+		})
+	else{
+	res.status(201).json({body})
+	}
+})
+app.put('/usuario/:id',function(req,res){
+	res.send({id:req.params.id,nombre:'Er Leví'})
+})
+app.delete('/usuario',function(req,res){
+	res.send('Hello word post')
+})
+app.listen(process.env.PORT,()=>{
+	console.log('Escuchando en 3000');
+})
